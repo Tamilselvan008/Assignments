7b4e95e574165f7a4eb42eef0f369ca0cb764344
Tamil Selvan <puretamilan17@gmail.com>
2025-04-27 23:29:48 +0530

    Assignments

--- src/assignments/Assignment8_Loop.java
+++ src/assignments/Assignment8_Loop.java
@@ -1,74 +1,69 @@
 package assignments;
 
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Assignment8_Loop {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub 
-	/*	Map<Integer,Integer> AmountCredit=new LinkedHashMap<Integer,Integer>();
-		AmountCredit.put(1, 50000);
-		AmountCredit.put(3, 3000);
-		AmountCredit.put(7, 4000);
-
-		Map<Integer,Integer> AmountDebit=new LinkedHashMap<Integer,Integer>();
-		AmountDebit.put(2, 2000);
-		AmountDebit.put(4, 15000);
-		AmountDebit.put(5, 200);
-		AmountDebit.put(6, 300);
-		AmountDebit.put(8, 3000);  
-		
-	*///	List<Map<Integer,Integer>> PostiveValue=new ArrayList<Map<Integer,Integer>>();
-		//PostiveValue.add(AmountCredit);
-	//	PostiveValue.add(AmountDebit); 
-		
-	//	for (Map<Integer,Integer> amount:PostiveValue ) {
-	//	System.out.println(amount);
-	//	}
-		
-		List<Integer> TransactionList=new ArrayList<Integer>();
-		TransactionList.add(1);
-		TransactionList.add(2);
-		TransactionList.add(3);
-		TransactionList.add(4);
-		TransactionList.add(5);
-		TransactionList.add(6);
-		TransactionList.add(7);
-		TransactionList.add(8);
-	//	System.out.println("Total Transaction: "+TransactionList.size());  	
-		for(Integer Trans:TransactionList) {
-			
-			System.out.println("Total Transaction: "+Trans);
-		}
-		
+	
 		List<Integer> CreditList=new ArrayList<Integer>();
 		CreditList.add(50000);
 		CreditList.add(3000);
-		CreditList.add(4000);
+		CreditList.add(4000); 
 		
-		int Credit=CreditList.get(0)+CreditList.get(1)+CreditList.get(2);
-        System.out.println("Total Number of Credits: "+Credit); 
+		int totalcredit=0;
+		int numbersofcredit=0;
+		int creditSuspicious=0;
+
+		for(Integer credit:CreditList) { 
+			
+			if(credit > 0) { 
+				numbersofcredit++;				
+			}
+			 if((credit>=10000)) {
+				
+				System.out.println("Suspicious credit transaction with Amount: "+credit);
+				creditSuspicious++;
+			} 
+			 totalcredit +=credit;
+		}
+		
+	
         
+        List<Integer> debitlist=new ArrayList<Integer>();
+        debitlist.add(-2000);
+        debitlist.add(-15000);
+        debitlist.add(-200);
+        debitlist.add(-300);
+        debitlist.add(-3000); 
         
-        
-        List<Integer> debitList=new ArrayList<Integer>();
-        debitList.add(-2000);
-        debitList.add(-15000);
-        debitList.add(-200);
-        debitList.add(-300);
-        debitList.add(-3000); 
-        
-        int Debit=debitList.get(0)+debitList.get(1)+debitList.get(2)+debitList.get(3)+debitList.get(4);
-        System.out.println("Total Number of Credits: "+Debit);
-        
-        System.out.println("Balance Amount in Account: "+(Credit+Debit));  
-        
-        
-        // Pending Loop
-        */
+		int totaldebit=0;
+		int numbersofdebit=0;
+		int debitSuspicious=0;
+		
+		for(Integer debit:debitlist) { 
+			
+			if(debit < 0) { 
+				numbersofdebit++;				
+			}
+			 if((debit <= -10000)) {
+				
+				System.out.println("Suspicious debit transaction with Amount: "+debit); 
+				debitSuspicious++;
+			} 
+			 totaldebit +=debit;
+		}
+		System.out.println("----------------------------------------------");
+		System.out.println("Total Credit Amount: "+totalcredit);
+		System.out.println("Total debit Amount: "+totaldebit);
+		System.out.println("Balance amount in account: "+(totalcredit+totaldebit));
+	    System.out.println("Overall Credit count: "+numbersofcredit);		
+	    System.out.println("Overall debit count: "+numbersofdebit);
+	    System.out.println("Total Suspicious count: "+(creditSuspicious+debitSuspicious));
+	    System.out.println("Total Transaction: "+(numbersofcredit+numbersofdebit));
+	     
+ 
 	}
 
 }
---
+++ src/assignments/Assignment9_LoopForHikeCalc.java
@@ -0,0 +1,20 @@
+package assignments;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Assignment9_LoopForHikeCalc {
+
+	public static void main(String[] args) {
+   
+		Map<String, String,String> employeeRow=new HashMap<String, String>();
+		employeeRow.put("Name","Tamizh");
+		employeeRow.put("BaseSalary","50000");
+		
+		
+		System.out.println(employeeRow);
+		
+		
+	}
+
+}
